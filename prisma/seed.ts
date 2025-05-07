ef79ddfcc61e15c3e9cc0e4e97838f2e47487169
Guille <guillermobernallou@gmail.com>
2025-05-07 14:00:09 +0200

    feat: complete prisma seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,6 +27,11 @@ async function main() {
         name: result["CENTRO EDUCATIVO"],
       },
     });
+
+    const escuela = await prisma.escuela.findFirst({
+      where: {name: result["CENTRO EDUCATIVO"]}
+    })
+
     await prisma.especialidad.upsert({
       where: { name: result.SKILL },
       update: {},
@@ -34,7 +39,44 @@ async function main() {
         name: result.SKILL,
       },
     })
-    await prisma
+
+    const especialidad = await prisma.especialidad.findFirst({
+      where: {name: result.SKILL}
+    })
+    
+    await prisma.competidor.upsert({
+      where: { name: result.COMPETIDOR },
+      update: {},
+      create: {
+        name: result.COMPETIDOR,
+        escuelaId: escuela?.id,
+        especialidadId: especialidad?.id
+      },
+    })
+
+    await prisma.edicion.upsert({
+      where: { year: result.AÑO },
+      update: {},
+      create: {
+        year: result.AÑO,
+      },
+    })
+
+    const edicion = await prisma.edicion.findFirst({
+      where: {year: result.AÑO}
+    })
+
+    const competidor = await prisma.competidor.findFirst({
+      where: {name: result.COMPETIDOR}
+    })
+
+    await prisma.resultadoCompetidor.create({
+      data: {
+        competidorId: competidor?.id!,
+        edicionId: edicion?.id!,
+        position: result.MEDALLA,
+      }
+    }) 
   }
 }
 
