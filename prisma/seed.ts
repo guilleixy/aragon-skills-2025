91bdc5a5b1358926d85fa0a6f1f543b19ee8d40b
Guille <guillermobernallou@gmail.com>
2025-05-07 10:39:44 +0200

    feat: start .json to db migration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-async function main() {}
+const prisma = new PrismaClient();
+
+import results from "../python/medallas-web.json";
+
+async function main() {
+  for (let result of results) {
+    await prisma.resultadoCompetidor.create({
+      data: result,
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
