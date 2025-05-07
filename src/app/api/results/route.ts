dd81c737ca046fcb4855eac5403b2fa456ad291e
Guille <guillermobernallou@gmail.com>
2025-05-07 11:48:26 +0200

    feat: start rest API, start auth

---
+++ src/app/api/results/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function POST(req: Request) {}
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const filters: any = {};
+    if (searchParams.has("name")) {
+      filters.competidor = {
+        name: searchParams.get("name") || undefined,
+      };
+    }
+    if (searchParams.has("escuela")) {
+      filters.driver = {
+        ...filters.competidor,
+        escuelaId: parseInt(searchParams.get("escuela")!),
+      };
+    }
+    if (searchParams.has("especialidad")) {
+      filters.driver = {
+        ...filters.competidor,
+        especialidadId: parseInt(searchParams.get("especialidad")!),
+      };
+    }
+    if (searchParams.has("posicion")) {
+      filters.driver = {
+        ...filters.competidor,
+        posicion: parseInt(searchParams.get("posicion")!),
+      };
+    }
+    const results = await prisma.resultadoCompetidor.findMany({
+      where: filters,
+      include: {
+        competidor: true,
+        edicion: true,
+      },
+    });
+
+    return NextResponse.json(results, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching results:", error);
+    return NextResponse.json(
+      { message: "Error fetching results" },
+      { status: 500 }
+    );
+  }
+}
